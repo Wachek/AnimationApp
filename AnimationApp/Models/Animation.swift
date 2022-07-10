8e8532a58a80e81b5e5fe0a683358d62479e1032
Viacheslav Turchak <wachek7@gmail.com>
2022-07-10 23:14:34 +0300

    final project

---
+++ AnimationApp/Models/Animation.swift
@@ -0,0 +1,16 @@
+//
+//  Animation.swift
+//  AnimationApp
+//
+//  Created by Вячеслав Турчак on 10.07.2022.
+//
+
+import Foundation
+
+struct Animation {
+    let name: String
+    let curve: String
+    let duration: Double
+    let force: Double
+    let delay: Double
+}
--- AnimationApp/ViewControllers/ViewController.swift
+++ AnimationApp/ViewControllers/ViewController.swift
@@ -5,21 +5,60 @@
 //  Created by Вячеслав Турчак on 08.07.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
-    @IBOutlet var animationView: UIView!
+    @IBOutlet var animationView: SpringView!
     @IBOutlet var animationLabel: UILabel!
+    @IBOutlet var animationButton: UIButton!
     
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//    }
+    var currentAnimation: Animation!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        currentAnimation = createRandomAnimation()
+        updateAnimationLabel(with: currentAnimation)
+        animationButton.setTitle("Run \(currentAnimation.name)", for: .normal)
+    }
 
-
-    @IBAction func animationButtonPressed(_ sender: UIButton) {
+    private func createRandomAnimation() -> Animation {
+        let name = Spring.AnimationPreset.allCases.randomElement()?.rawValue ?? ""
+        let curve = Spring.AnimationCurve.allCases.randomElement()?.rawValue ?? ""
+        let duration = Double.random(in: 1...3)
+        let force = Double.random(in: 0.5...1.5)
+        let delay = Double.random(in: 0.5...1)
+        
+        let animation = Animation(
+            name: name,
+            curve: curve,
+            duration: duration,
+            force: force,
+            delay: delay)
+        
+        return animation
     }
     
+    private func updateAnimationLabel(with animation: Animation) {
+        animationLabel.text = "Animation: \(animation.name) \nCurve: \(animation.curve) \nDuration: \(String(format: "%.2f", animation.duration)) \nForce: \(String(format: "%.2f", animation.force)) \nDelay: \(String(format: "%.2f", animation.delay))"
+    }
+
+    @IBAction func animationButtonPressed(_ sender: UIButton) {
+        
+        animationView.animation = currentAnimation.name
+        animationView.curve = currentAnimation.curve
+        animationView.force = CGFloat(currentAnimation.force)
+        animationView.duration = CGFloat(currentAnimation.duration)
+        animationView.delay = CGFloat(currentAnimation.delay)
+        animationView.animate()
+        
+        updateAnimationLabel(with: currentAnimation)
+        
+        currentAnimation = createRandomAnimation()
+        
+        sender.setTitle("Run \(currentAnimation.name)", for: .normal)
+    }
 }
+
 
